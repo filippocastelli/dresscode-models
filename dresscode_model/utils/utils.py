e5e4ec66ae9fa903d4e65bee91fe2b40ba9cd8e6
Filippo Castelli <filippocastelli42@gmail.com>
2023-08-31 13:07:33 +0200

    utils typing

--- dresscode_model/utils/utils.py
+++ dresscode_model/utils/utils.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 import torch
 
-def save_checkpoint(model: torch.Module, save_path: Path, use_cuda: bool = True):
+def save_checkpoint(model: torch.nn.Module, save_path: Path, use_cuda: bool = True):
 
     if not save_path.parent.exists():
         save_path.parent.mkdir(parents=True)
@@ -11,7 +11,7 @@ def save_checkpoint(model: torch.Module, save_path: Path, use_cuda: bool = True)
         model.cuda()
 
 
-def load_checkpoint(model: torch.Module, checkpoint_path: Path, use_cuda: bool = True):
+def load_checkpoint(model: torch.nn.Module, checkpoint_path: Path, use_cuda: bool = True):
 
     if not checkpoint_path.exists():
         raise FileNotFoundError(f"Checkpoint file not found at {checkpoint_path}")
